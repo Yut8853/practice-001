558704272d8871ddfaef774d61246353afb55742
Yut8853 <hello@junkbranding.com>
2025-08-02 08:41:55 +0900

    微調整、ボイラープレート追加

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,29 @@
-// eslint.config.ts
-import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
+import parser from '@typescript-eslint/parser';
+import plugin from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-config-prettier';
 
 export default defineConfig([
   {
     files: ['**/*.{ts,tsx}'],
-    ...tseslint.configs.recommended,
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': plugin as any,
+    },
+    rules: {
+      ...plugin.configs.recommended.rules,
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx,js}'],
+    rules: {
+      ...prettier.rules,
+    },
   },
 ]);
